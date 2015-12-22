36e9eab90508687df585db8e62edd57813dd6f3f
Neil White <stigfinnaren@icloud.com>
2015-12-22 15:19:05 +1000

    added a button and broke it

--- ReadPOAMA/ViewController.swift
+++ ReadPOAMA/ViewController.swift
@@ -10,15 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("Commit for GitHub")
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func GetMaxT(sender: UIButton) {
+        let url = "http://www.google.com"
+        print(url)
     }
 
 
